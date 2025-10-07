4d3421e4642ff93aee60214443af2838b2092437
Paulo Soares <paulinho-freitas@live.com>
2025-10-07 19:26:57 -0300

    validação de listar sem autenticacao

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post("/cadastro", async (req, res) => {
   try {
@@ -23,16 +23,16 @@ router.post("/cadastro", async (req, res) => {
     });
     res.status(201).json(userDB);
   } catch (error) {
-    res.status(500).json({ message: "Erro no Servidor, tente novamente, CADASTRO" });
+    res
+      .status(500)
+      .json({ message: "Erro no Servidor, tente novamente, CADASTRO" });
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
-
     //guardando informações do usuário no front
     const userInfo = req.body;
-
 
     //buscando usuário no banco
     const user = await prisma.user.findUnique({
@@ -40,34 +40,37 @@ router.post("/login", async (req, res) => {
     });
 
     // mensagem de falha ao encontrar usuário
-    if(!user){
-        return res.status(404).json({message: 'Usuário não encontrado'})
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
     }
 
     //comparando as senhas
-    const isMatch = await bcrypt.compare(userInfo.password, user.password)
-    
-    
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
     // mensagem de falha.
-    if(!isMatch){
-        return res. status(400).json({message: 'Senha inválida'})
+    if (!isMatch) {
+      return res.status(400).json({ message: "Senha inválida" });
     }
 
     //gerando token // 1° payload, secret, options
-    const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '1d'})
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
 
-    res.status(200).json(token)
+    res.status(200).json(token);
   } catch (error) {
-    res.status(500).json({ message: "Erro no Servidor, tente novamente, LOGIN" });
+    res
+      .status(500)
+      .json({ message: "Erro no Servidor, tente novamente, LOGIN" });
   }
 });
 
-router.get("/listar", async (req, res) => {
-  
-        //buscando usuário no banco
-    const user = await prisma.user.findMany()
-    res.status(200).json(user)
-  
-});
+router.get('/listar-usuarios', async (req, res) => {
+    try {
+        const user = await prisma.user.findMany()
+
+        res.status(200).json({message: 'Usuários listados com sucesso', user})
+    } catch (error) {
+        res.status(500).json({message: 'Falha no servidor, AUTH/LISTAR'})
+    }
+})
 
 export default router;
