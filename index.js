e9f54c9ada0e20f46daec0b43f1fbf7b501a19ee
Paulo Soares <102790702+DevPauloS@users.noreply.github.com>
2025-10-12 01:19:01 -0300

    Update index.js
    correção

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ app.use(cors({
   credentials: true
 }));
 
+router.get("/listar-usuarios", async (req, res) => {
+  try {
+    const user = await prisma.user.findMany();
 
+     res.status(200).json({ message: "Usuários listados com sucesso", user });
+  } catch (error) {
+     res.status(500).json({ message: "Falha no servidor, LISTAR"});
+  }
+});
 // const port = process.env.port || 3001;
 
-app.use('/', publicRoutes)
+// app.use('/', publicRoutes)
 // app.use('/', auth, privateRoutes)
 
 app.listen(3000, () => console.log("Servidor rodando!!"))
