9c41dc92bf7c9b29d96e4989bbc09dfb9f2c1848
Paulo Soares <102790702+DevPauloS@users.noreply.github.com>
2025-10-13 15:44:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 
 const app = express();
 app.use(express.json())
-app.use(cors('https://paulosf21.netlify.app'))
+// app.use(cors('https://paulosf21.netlify.app'))
 
 app.use('/', publicRoutes)
 app.use('/', auth, privateRoutes)
