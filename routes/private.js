d9fe2a424352acfc1bc7a679091752c38335c46f
Paulo Soares <paulinho-freitas@live.com>
2026-02-20 00:19:33 -0300

    Filtrando o retorno do BD para garantir a mascara dos dados e atender a lei LGPD

--- routes/private.js
+++ routes/private.js
@@ -6,18 +6,16 @@ const prisma = new PrismaClient();
 
 router.get("/listar-usuarios", async (req, res) => {
   try {
-    const user = await prisma.user.findMany();
-    const userFiltrado = 
-        user.map((user) => {
-          return {
-            id: user.id * 123,
-            name: user.name,
-            email: user.email
-          }
-        });
+    const user = await prisma.user.findFirst({
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    });
     res
       .status(200)
-      .json({ message: "Usuários listados com sucesso", user: userFiltrado});
+      .json({ message: "Usuários listados com sucesso", user: user});
   } catch (error) {
     res.status(500).json({ message: "Falha no servidor" });
   }
